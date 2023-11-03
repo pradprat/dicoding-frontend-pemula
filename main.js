42c0ec4e5c5a19f1845af6c1eb8fe86ddb09608d
Pradika Fitra Pratama <pradikafp14@gmail.com>
2023-11-04 00:47:11 +0700

    add search

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("test");
     onAddBook();
   });
+  const searchForm = document.getElementById("searchBook");
+  searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    onSearchBook();
+  });
+  render();
 });
 
 const onAddBook = () => {
@@ -53,16 +59,18 @@ const getReadBooks = () => {
 const renderUnreadBooks = () => {
   const books = getUnreadBooks();
   const unreadBookList = document.getElementById("incompleteBookshelfList");
+  unreadBookList.innerHTML = "";
   books.forEach((book) => {
-    const bookItem = createBookItem(book);
+    const bookItem = createUnreadBookItem(book);
     unreadBookList.innerHTML += bookItem;
   });
 };
 const renderReadBooks = () => {
   const books = getReadBooks();
   const readBookList = document.getElementById("completeBookshelfList");
+  readBookList.innerHTML = "";
   books.forEach((book) => {
-    const bookItem = createBookItem(book);
+    const bookItem = createReadBookItem(book);
     readBookList.innerHTML += bookItem;
   });
 };
@@ -70,8 +78,39 @@ const render = () => {
   renderUnreadBooks();
   renderReadBooks();
 };
+const onAddBookToRead = (id) => {
+  const books = getUnreadBooks();
+  const book = books.filter((book) => book.id == id)[0];
+  saveReadBook(book);
+  removeUnreadBook(id);
+  render();
+};
+const onRemoveBook = (id) => {
+  removeUnreadBook(id);
+  removeReadBook(id);
+  render();
+};
+const removeUnreadBook = (id) => {
+  const books = getUnreadBooks();
+  const filteredBooks = books.filter((book) => book.id != id);
+  localStorage.setItem("unread_books", JSON.stringify(filteredBooks));
+  render();
+};
+const removeReadBook = (id) => {
+  const books = getReadBooks();
+  const filteredBooks = books.filter((book) => book.id != id);
+  localStorage.setItem("read_books", JSON.stringify(filteredBooks));
+  render();
+};
+const onAddBookToUnread = (id) => {
+  const books = getReadBooks();
+  const book = books.filter((book) => book.id == id)[0];
+  saveUnreadBook(book);
+  removeReadBook(id);
+  render();
+};
 
-const createBookItem = (book) => {
+const createUnreadBookItem = (book) => {
   return `
   <article class="book_item">
     <h3>${book.title}</h3>
@@ -79,9 +118,38 @@ const createBookItem = (book) => {
     <p>Tahun: ${book.year}</p>
 
     <div class="action">
-      <button class="green">Belum selesai di Baca</button>
-      <button class="red">Hapus buku</button>
+      <button onclick="onAddBookToRead(${book.id})" class="green" data-id=${book.id}>Selesai di Baca</button>
+      <button onclick="onRemoveBook(${book.id})" class="red" data-id=${book.id}>Hapus buku</button>
     </div>
   </article>
   `;
+};
+
+const createReadBookItem = (book) => {
+  return `
+  <article class="book_item">
+    <h3>${book.title}</h3>
+    <p>Penulis: ${book.author}</p>
+    <p>Tahun: ${book.year}</p>
+
+    <div class="action">
+      <button onclick="onAddBookToUnread(${book.id})" class="green" data-id=${book.id}>Belum selesai di Baca</button>
+      <button onclick="removeReadBook(${book.id})" class="red" data-id=${book.id}>Hapus buku</button>
+    </div>
+  </article>
+  `;
+};
+
+const onSearchBook = () => {
+  const keyword = getValue("searchBookTitle");
+  const books = getUnreadBooks();
+  const filteredBooks = books.filter((book) => {
+    return book.title.toLowerCase().includes(keyword.toLowerCase());
+  });
+  const unreadBookList = document.getElementById("incompleteBookshelfList");
+  unreadBookList.innerHTML = "";
+  filteredBooks.forEach((book) => {
+    const bookItem = createUnreadBookItem(book);
+    unreadBookList.innerHTML += bookItem;
+  });
 };
